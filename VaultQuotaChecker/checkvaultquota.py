39401ff7ec4e81d70d45f7bd595f577d9386dec4
George Crary <craryg@onid.oregonstate.edu>
2016-10-07 14:14:56 -0700

    Add resetKVMQuotaCount, buildKeyMapUrl and update getKVMQuotaCount
    Add resetKVMQuotaCount, buildKeyMapUrl and update getKVMQuotaCount

--- VaultQuotaChecker/checkvaultquota.py
+++ VaultQuotaChecker/checkvaultquota.py
@@ -8,38 +8,73 @@ def sendNotificationEmail(count,job_interval='hour'):
     Sends the quota violation notification email.
     """
     #TODO Configure past TIME_INTERVAL and leave that to Jenkins
+    #TODO NOCHECKIN Requires testing
     text = 'The Vault API has had ' + str(count) + ' quota violaitons in the past {interval}'.format(interval=job_interval)
     msg = MIMEText(text,'plain')
     msg['Subject'] = 'Jenkins Vault Quota Checker Notification'
     msg['From'] = 'ECS DATA Jenkins Instance'
     msg['To'] = 'craryg@oregonstate.edu'
+    #TODO have jenkin's pass in a recipients list'
 
     #This line requires running this command 'python -m smtpd -n -c DebuggingServer localhost:1025'
     #TODO Streamline this
     s = smtplib.SMTP('localhost',1025)
     s.sendmail()
 
-def resetKVMQuotaCount():
-    #TODO FINISH IMPLEMENTING RESET CALL TO KVM.
+def resetKVMQuotaCount(keymap_url, config_data):
+    """
+    Executes a post request to the KVM to reset the quota violation count to 0.
+    Returns true on request sucess
+    """
+    jsonbody = json.dumps({'name': 'QVCK_1', 'value': 0})
+    r = requests.post(keymap_url, data=jsonbody, auth=(config_data['client_id'],config_data['client_secret']))
+    return r.status_code == 200
 
-def getKVMQuotaCount(config_data):
-    keymap_url = 
-    r = requests.get(keymap_url, auth=(config_data['client_id'],config_data['client_secret']]))
-    res = r.json()
-    return int(res[u'value'].decode('utf-8'))
+
+def buildKeyMapUrl(config_data):
+    """
+    Formats the KVM Keymap url for us using the config data.
+    """
+    s ='{hostname}/{version}/organizations/{org}/environments/{env_name}/keyvaluemaps/{map_name}/entries/{entry_name}'
+    return s.format(
+        hostname    = config_data['hostname'],
+        version     = config_data['version'],
+        org         = config_data['org'],
+        env_name    = config_data['env_name'],
+        map_name    = config_data['map_name'],
+        entry_name  = config_data['entry_name']
+    )
+
+
+def getKVMQuotaCount(keymap_url, config_data):
+    """
+    Executes a get request to the KVM Quota count entry
+    Returns status code and val as a pair for error handling purposes.
+    """
+    r = requests.get(keymap_url, auth=(config_data['client_id'],config_data['client_secret']))
+    if r.status_code == 200:
+        res = r.json()
+        val = int(res[u'value'].decode('utf-8'))
+    else:
+        val = None
+    return (r.status_code, val)
+
     #TODO Try to trash this with invalid responses from the server
 
 if __name__ == '__main__':
     try:
         # Read configuration file in JSON format
-        keymap_url = https://api.enterprise.apigee.com/v1/organizations/osu/environments/test/keyvaluemaps/GeorgeLookupJson_1_QVCKVM/entries/QVCK_1
+        #Add Jenkins job string parameter for job interval
         config_data_file = open(sys.argv[1])
         config_data = json.load(config_data_file)
-        count = getKVMQuotaCount(config_data)
+        keymap_url = buildKeyMapUrl()
 
-        if count > 0:
+        status, val = getKVMQuotaCount(keymap_url, config_data)
+        if status == requests.codes.ok
+            print "Quota value recieved, sending email."
+        else:
+            print "Get request to KVM failed."
 
     except:
         print "Please make sure placing the configuration file in the same directory and pass it as an argument!"
-    
 	#getQuotaViolationsCount('https://api.enterprise.apigee.com/v1/organizations/osu/keyvaluemaps/GeorgeLookupJson_1_QVCKVM',)
